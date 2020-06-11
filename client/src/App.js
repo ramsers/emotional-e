f2e78087de733e61866345f4eea84e89d2fb54f7
Raheim <raheimbbailey@gmail.com>
2020-06-11 00:34:02 -0400

    add mixins and partials, add header mobile styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './App.scss';
 import HomePage from './HomePage/HomePage'
+import Header from './Header/Header';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter className="App">
-
-      <Switch>
-        <Route path='/' exact component={HomePage}/>
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter className="App">
+        <Header/>
+        <Switch>
+          <Route path='/' exact component={HomePage}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,12 +1,27 @@
-import react from 'react';
+import React from 'react';
+import './Header.scss';
+import logo from './HeaderAssets/Logo.png';
 import {NavLink, Link } from 'react-router-dom';
 
 const Header =() => {
     return(
         <header className="header">
             <div className="header__wrap">
+                <Link to="/" className="header__logo">
+                    <img src={logo} className="header__logo-img" alt="emotional-e logo"/>
+                </Link>
 
+                <nav className="header__nav">
+                    <NavLink to="/journal" className="header__nav-link">
+                        Notes
+                    </NavLink>
+                    <NavLink to="/clicks" className="header__nav-link">
+                        Data
+                    </NavLink>
+                </nav>
             </div>
         </header>
     )
 }
+
+export default Header;
