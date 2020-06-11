08513163c3ee4950631adc9d1318887a2114846f
Raheim <raheimbbailey@gmail.com>
2020-06-10 23:41:07 -0400

    add base folder architecture

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,11 +1,11 @@
 import react from 'react';
-import {Link } from 'react-router-dom';
+import {NavLink, Link } from 'react-router-dom';
 
 const Header =() => {
     return(
         <header className="header">
             <div className="header__wrap">
-                
+
             </div>
         </header>
     )
