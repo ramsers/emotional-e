722711b9ee079310bf3b8d278b01aee781e15abe
Raheim <raheimbbailey@gmail.com>
2020-06-19 02:44:56 -0400

    add homepage card animations

--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -37,11 +37,12 @@ useEffect(() => {
              console.log(err)
          })
         gsap.to(emotionHead, {
-            scrollTrigger: emotionHead,
+            scrollTrigger: emotionsCardSection,
             opacity:1,
                 x: 0,
                 ease:Power3.eastOut,
-                duration: 1.3   
+                duration: 1.3,  
+                delay: .2 
         })
         gsap.to(emotionsCard, {
             scrollTrigger: emotionHead,
@@ -49,7 +50,7 @@ useEffect(() => {
             y: 0,
             ease:Power3.eastOut,
             duration: 1,
-            delay: .2
+            delay: .35
         })
         gsap.to(emotionsCard2, {
             scrollTrigger: emotionHead,
@@ -57,7 +58,7 @@ useEffect(() => {
             y: 0,
             ease:Power3.eastOut,
             duration: 1,
-            delay: .2
+            delay: .35
         })
         gsap.to(emotionsCard3, {
             scrollTrigger: emotionHead,
@@ -65,7 +66,7 @@ useEffect(() => {
             y: 0,
             ease:Power3.eastOut,
             duration: 1,
-            delay: .2
+            delay: .35
         })
         
 }
@@ -100,6 +101,14 @@ useEffect(() => {
                 <div  className="emotions__wrap">
 
                     <Link  className="emotions__card-link emotions--anxiety" ref={el => {emotionsCard = el}} onClick={anxietyClick} to='/anxiety'>
+                            <p className="emotions__card-desc">Everyone feels anxious now and then. It’s a normal emotion. 
+                            For example, you may feel nervous when faced with a problem at work, 
+                            before taking a test, or before making an important decision. Anxiety disorders a little different. 
+                            They are a group of mental illnesses, and the distress they cause can 
+                            keep you from carrying on with your life normally. For people who have one, worry and fear are constant and overwhelming, and this can be disabling. 
+                            But with treatment, many people can manage those feelings and get back to a fulfilling life.
+                            </p>
+
                         <div  className="emotions__card"></div>
                         <div className="emotions__card-title-ctn">
                                 <h2 className="emotions__card-title">Anxiety</h2>
@@ -108,6 +117,14 @@ useEffect(() => {
                     
                     
                     <Link className="emotions__card-link emotions--anger" ref={el => {emotionsCard2 = el}} onClick={angerClick} to='/anger'>
+                    <p className="emotions__card-desc">Everyone feels anxious now and then. It’s a normal emotion. 
+                            For example, you may feel nervous when faced with a problem at work, 
+                            before taking a test, or before making an important decision. Anxiety disorders a little different. 
+                            They are a group of mental illnesses, and the distress they cause can 
+                            keep you from carrying on with your life normally. For people who have one, worry and fear are constant and overwhelming, and this can be disabling. 
+                            But with treatment, many people can manage those feelings and get back to a fulfilling life.
+                            </p>
+
                         <div className="emotions__card"></div>
                         <div className="emotions__card-title-ctn">
                                 <h2 className="emotions__card-title">Anger</h2>
@@ -116,6 +133,15 @@ useEffect(() => {
                     
                    
                    <Link className="emotions__card-link emotions--depression" ref={el => {emotionsCard3 = el}} onClick={depressionClick} to='/depression'>
+                        
+                   <p className="emotions__card-desc">Everyone feels anxious now and then. It’s a normal emotion. 
+                            For example, you may feel nervous when faced with a problem at work, 
+                            before taking a test, or before making an important decision. Anxiety disorders a little different. 
+                            They are a group of mental illnesses, and the distress they cause can 
+                            keep you from carrying on with your life normally. For people who have one, worry and fear are constant and overwhelming, and this can be disabling. 
+                            But with treatment, many people can manage those feelings and get back to a fulfilling life.
+                            </p>
+
                         <div className="emotions__card"></div>
                         <div className="emotions__card-title-ctn">
                                 <h2 className="emotions__card-title">Depression</h2>
